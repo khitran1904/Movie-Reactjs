39487112291c6ced27f91fd87e0b265abeebbab0
Nguyen Le Huy Bao <nguyenlehb1808@gmail.com>
2021-03-02 15:39:25 +0700

    use formik and yup for sign in

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -5,28 +5,24 @@ import { Redirect } from "react-router-dom";
 import "./style.css";
 import { loginAction } from "../../Action/Auth";
 import Signup from "../Signup";
+import { Formik } from "formik";
+import * as Yup from "yup";
 export default function Login() {
   const dispatch = useDispatch();
-  const [form, setForm] = useState({ taiKhoan: "", matKhau: "" });
-  const [submittedSignin, setSubmittedSignin] = useState(false);
+  const form = { taiKhoan: "", matKhau: "" };
   const { currentUser, error, loading } = useSelector(
     (state) => state.authReducer
   );
 
-  const handleLoginInputs = (event) => {
-    setForm({
-      ...form,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleSubmitLogin = (e) => {
-    e.preventDefault();
-    setSubmittedSignin(true);
-    if (form.taiKhoan && form.matKhau) {
-      dispatch(loginAction(form));
-    }
-  };
+  const validationSchema = Yup.object({
+    taiKhoan: Yup.string()
+      .required("Username is required")
+      .min(5, "Username must have at least 5 characters")
+      .max(10, "Username must have max 14 characters"),
+    matKhau: Yup.string()
+      .required("Password is required")
+      .min(6, "Password is too short - should be 6 chars minimum"),
+  });
 
   if (currentUser) {
     return <Redirect to="/" />;
@@ -35,60 +31,69 @@ export default function Login() {
     <div className="login">
       <div className="login__form mx-auto text-center">
         <span className="form__header">Login here</span>
-        <form onSubmit={handleSubmitLogin}>
-          <div
-            className={
-              "form-group" + (submittedSignin && !form.taiKhoan)
-                ? "has-error"
-                : ""
-            }
-          >
-            <input
-              name="taiKhoan"
-              placeholder="USERNAME"
-              onChange={handleLoginInputs}
-              type="text"
-            />
-            {submittedSignin && !form.taiKhoan && (
-              <div className="text-danger">Username is required</div>
-            )}
-          </div>
-          <div
-            className={
-              "form-group" + (submittedSignin && !form.taiKhoan)
-                ? "has-error"
-                : ""
-            }
-          >
-            <input
-              name="matKhau"
-              placeholder="PASSWORD"
-              onChange={handleLoginInputs}
-              type="password"
-            />
-            {submittedSignin && !form.matKhau && (
-              <div className="text-danger">Password is required</div>
-            )}
-          </div>
-          <div className="form-group">
-            <button
-              disabled={loading}
-              className="btn btn-success"
-              type="submit"
-            >
-              LOG IN
-            </button>
-            {loading && (
-              <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-            )}
-          </div>
+        <Formik
+          initialValues={form}
+          validationSchema={validationSchema}
+          onSubmit={(values) => dispatch(loginAction(values))}
+        >
+          {({
+            handleSubmit,
+            handleBlur,
+            handleChange,
+            values,
+            errors,
+            touched,
+            isValid,
+            dirty,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  name="taiKhoan"
+                  placeholder="USERNAME"
+                  type="text"
+                  value={values.taiKhoan}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.taiKhoan && touched.taiKhoan && (
+                  <div className="text-danger">{errors.taiKhoan}</div>
+                )}
+              </div>
+              <div className="form-group">
+                <input
+                  name="matKhau"
+                  placeholder="PASSWORD"
+                  type="password"
+                  value={values.matKhau}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.matKhau && touched.matKhau && (
+                  <div className="text-danger">{errors.matKhau}</div>
+                )}
+              </div>
+              <div className="form-group">
+                <button
+                  disabled={!(isValid && dirty)}
+                  className="btn btn-success"
+                  type="submit"
+                >
+                  LOG IN
+                </button>
+                {loading && (
+                  <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                )}
+              </div>
 
-          {error ? (
-            <div className="alert alert-danger">
-              <span>{error}</span>
-            </div>
-          ) : null}
-        </form>
+              {error ? (
+                <div className="alert alert-danger">
+                  <span>{error}</span>
+                </div>
+              ) : null}
+            </form>
+          )}
+        </Formik>
         <Signup />
       </div>
     </div>
--- src/Pages/Signup/index.js
+++ src/Pages/Signup/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { signUpAction, closeAction } from "../../Action/Auth";
 import { Formik } from "formik";
