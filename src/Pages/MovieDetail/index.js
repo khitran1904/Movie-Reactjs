8eb56976175fecae59aa89950d0ecb7fa115f6bc
Nguyen Le Huy Bao <nguyenlehb1808@gmail.com>
2021-03-13 22:38:30 +0700

    complete loading page

--- src/Pages/MovieDetail/index.js
+++ src/Pages/MovieDetail/index.js
@@ -2,22 +2,30 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovieDetailAction } from "../../Action/Movie";
 import LoadingPage from "../../Components/Loading";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import ReactStars from "react-rating-stars-component";
+import YoutubePopup from "../../Components/YtbPopup";
+import "react-circular-progressbar/dist/styles.css";
 import "./style.css";
 export default function MovieDetail(props) {
   const dispatch = useDispatch();
+  const percentage = 66;
   const { movieDetail, loading } = useSelector((state) => {
     return state.movieReducer;
   });
+
   useEffect(() => {
     dispatch(getMovieDetailAction(props.match.params.movieId));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
 
-  // eslint-disable-next-line no-unused-vars
+  const handleRenderStarRating = (rates) => {
+    for (let i = 0; i < rates / 2; i++) {}
+  };
 
   if (loading) {
     return <LoadingPage />;
   }
+
   return (
     <div className="">
       <div
@@ -25,26 +33,57 @@ export default function MovieDetail(props) {
         style={{
           backgroundColor: "black",
           color: "white",
+          backgroundImage: `url(${movieDetail.hinhAnh})`,
         }}
       >
-        <div className="movie-img">
-          <img src={movieDetail.hinhAnh} className="img-fluid" alt="" />
-        </div>
+        <div className="bg-overlay"></div>
+        <div className="movie-img"></div>
         <div className="movie-info row">
           <div className="movie-poster col-3">
-            <img src={movieDetail.hinhAnh} className="img-fluid" alt="" />
+            <div className="movie-poster-main">
+              <img src={movieDetail.hinhAnh} className="" alt="" />
+            </div>
           </div>
           <div className="movie-main-info col-5">
             <p className="movie-date">
               {movieDetail.ngayKhoiChieu?.split("T")[0]}
             </p>
             <p className="movie-name">{movieDetail.tenPhim?.toUpperCase()}</p>
-            <p className="movie-desc">{movieDetail?.moTa}</p>
+
+            <p className="movie-time"></p>
           </div>
-          <div className="movie-rating col-2">Movie rating</div>
+          <div className="movie-rating col-2">
+            <CircularProgressbar
+              strokeWidth={4}
+              value={movieDetail.danhGia}
+              maxValue={10}
+              text={`${movieDetail.danhGia}`}
+              styles={buildStyles({
+                textSize: "40px",
+                textColor: "white",
+                pathColor: "#7ed321",
+                trailColor: "#3a3a3a",
+              })}
+            />
+
+            <ReactStars
+              classNames="star-ratings"
+              size={22}
+              count={5}
+              color={"white"}
+              activeColor={"red"}
+              value={movieDetail.danhGia / 2}
+              isHalf={true}
+              emptyIcon={<i className="far fa-star" />}
+              halfIcon={<i className="fa fa-star-half-alt" />}
+              filledIcon={<i className="fa fa-star" />}
+              edit={false}
+            />
+          </div>
         </div>
-        <div className="movie-rating col-5">Movie Rating</div>
+        <div className="movie-rating col-5"></div>
       </div>
+      <YoutubePopup trailer={movieDetail.trailer} />
     </div>
   );
 }
